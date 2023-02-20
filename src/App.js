0bd69fb6ca159457eca518e3028b81c3a2a82c5a
kayre <kayresantos@gmail.com>
2023-02-20 02:11:12 -0800

    majority done need to add styling

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// import logo from './logo.svg';
+import './App.css';
+import MyComponent from "./buy_sell";
+function App() {
+  return (
+    <div className='App-header'>
+      <MyComponent />
+    </div>
+  );
+
+}
+
+export default App;
---
+++ src/buy_sell.js
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+import './styles.css';
+
+function Row({ size, price, color}) {
+    const priceColor = `price ${color}`;
+    return(
+        <div className='column'>
+            <span className='size-color'>{size}</span>
+            <span className={priceColor}>{price}</span>
+        </div>
+    );
+}
+
+function MyComponent() {
+    //Use the useState hook to define the rows state
+    const [rows, setRows] = useState([]);
+
+    //Define a function that handles the Buy button click
+    const handleBuyClick = () => {
+        //generate a new row object with a random buy and sell values
+        const newRows = [...rows, {size: Math.random(), price: 3700 + Math.random() * 2000, color: 'green'}];
+        // Update the rows state with the new row object
+        setRows(newRows);
+    };
+    const handleSellClick = () => {
+        //generate a new row object with a random buy and sell values
+        const newRows = [...rows, {size: Math.random(), price: 3700 + Math.random() * 2000, color: 'red'}];
+        // Update the rows state with the new row object
+        setRows(newRows);
+    };
+    return (
+        <div>
+            <button onClick={handleBuyClick} className='buy-button'>Buy</button>
+            <button onClick={handleSellClick} className='sell-button'>Sell</button>
+            <div className='table'>
+             {/* Map over the rows state and render each row with the Row component */}
+             {rows.map((row, index) => (
+                <div key={index}>
+                    <Row {...row} />
+                </div>
+             ))}
+            </div>
+        </div>
+    );
+}
+
+export default MyComponent;
